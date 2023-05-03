754e053889426fb1ac640edf509b84ed4be69b86
obenchero <othmanebenchekroun@outlook.fr>
2023-05-03 16:12:33 +0100

    add deployment script

--- script_deployment.sh
+++ script_deployment.sh
@@ -2,20 +2,14 @@
 
 declare -a DOCKER_IMAGE_NAMES=("<docker-image-name-1>" "<docker-image-name-2>" "<docker-image-name-3>")
 
-if ! command -v docker &> /dev/null
+if ! command -v docker &> /dev/null && ! command -v docker-compose &> /dev/null
 then
-    echo -e "\e[31m Error: Docker is not installed or running. Please install Docker and start it before continuing. \e[0m"
-    exit
-fi
-
-if ! command -v docker-compose &> /dev/null
-then
-    echo -e "\e[31m Error: Docker Compose is not installed. Please install Docker Compose before continuing. \e[0m"
+    echo -e "\e[31m Error: Neither Docker nor Docker Compose is installed or running. Please install Docker and Docker Compose and start them before continuing. \e[0m"
     exit
 fi
 
 if [ $(docker-compose ps -q) ]; then
-    echo -e "\e[32m The Docker Compose is already running. \e[0m"
+    echo -e "\e[33m The Docker Compose is already running. \e[0m"
 else
     if [ ! -f docker-compose.yml ]; then
         echo -e "\e[31m Error: the docker-compose.yml file cannot be found. Please make sure the file exists in the current directory. \e[0m"
@@ -27,11 +21,9 @@ else
         if [ $(docker images -q $DOCKER_IMAGE_NAME) ]; then
             echo -e "\e[33m The Docker image $DOCKER_IMAGE_NAME has already been built. \e[0m"
             exit
-        else
-            if [ $(docker ps -q -f ancestor=$DOCKER_IMAGE_NAME) ]; then
-                echo -e "\e[33m A Docker container is already running for the image $DOCKER_IMAGE_NAME. \e[0m"
-                exit
-            fi
+        elif [ $(docker ps -q -f ancestor=$DOCKER_IMAGE_NAME) ]; then
+            echo -e "\e[33m A Docker container is already running for the image $DOCKER_IMAGE_NAME. \e[0m"
+            exit
         fi
     done
     
