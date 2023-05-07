ef41e095ba65e248fabb1bfc3d8ed1fe1c6e8e6f
obenchero <othmanebenchekroun@outlook.fr>
2023-05-07 18:47:00 +0100

    auth controller added

--- api/controllers/authentificationUserController.js
+++ api/controllers/authentificationUserController.js
@@ -4,11 +4,11 @@ const bcrypt = require('bcryptjs');
 
 const getAuthentificationToken = async (req, res) => {
     try {
-        if (req.body.username && !req.body.password) return res.status(400).json({ message: 'Username and password are required' });
+        if (!req.body.username || !req.body.password) return res.status(400).json({ message: 'Username and password are required' });
         const user = await User.findOne({ username: req.body.username });
         if (!user && typeof user !== typeof object) return res.status(400).json({ message: 'Username or password is incorrect' });
         bcrypt.compare(req.body.password, user.password, (err, match) => {
-            if (err) throw new Error(`Unexpected error comitted in ${err}`);
+            if (err) console.log(err);
             if (!match) {
                 return res.status(400).json({ message: 'Username or password is incorrect' });
             }
@@ -20,6 +20,7 @@ const getAuthentificationToken = async (req, res) => {
 
     } catch (error) {
         res.status(500).json({ message: 'Internal Server Error, we might patch up all of this occasional error later on.' });
+        console.log(error);
     }
 };
 
